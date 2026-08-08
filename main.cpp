d9801f17258526e55e260f2b76bcba20c33484fc
Daniel Huynh <danielhuynh082@gmail.com>
2026-08-08 12:15:36 -0700

    Show file sizes

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <filesystem>
 #include <cstdlib>
+#include <iomanip>
 
 
 
@@ -15,6 +16,22 @@ using namespace std;
 
 namespace fs = filesystem;
 
+
+string formatSize(uintmax_t bytes) {
+    const double KB = 1024.0;
+    const double MB = KB * 1024.0;
+    const double GB = MB * 1024.0;
+
+    if (bytes >= GB)
+        return to_string(bytes / GB) + " GB";
+    else if (bytes >= MB)
+        return to_string(bytes / MB) + " MB";
+    else if (bytes >= KB)
+        return to_string(bytes / KB) + " KB";
+    else
+        return to_string(bytes) + " bytes";
+}
+/*
 void printFolder(const fs::path dir_path){
     //ignore trash
     if(dir_path.filename() == ".Trash") return;
@@ -27,7 +44,30 @@ void printFolder(const fs::path dir_path){
        
     }
 }
+*/
+void printFolder(const string dir_path){
+    const fs::path path{dir_path};
+    //ignore trash
+    if(path.filename() == ".Trash") return;
+    
+    cout <<"\n< -- Interating Through Folder: " << dir_path << " -- >\n\n";
+  
 
+    for(auto const & dir_entry : fs::directory_iterator{path}){
+         if (dir_entry.is_regular_file()) {
+            cout << "File: "
+                 << dir_entry.path().filename()
+                 << " | "
+                 << formatSize(dir_entry.file_size())
+                 << '\n';
+        }
+        else if (dir_entry.is_directory()) {
+            cout << "Folder: "
+                 << dir_entry.path().filename()
+                 << '\n';
+        }
+    }
+}
 void checkDir(){
     //Get path and set entry to directory
    // const fs::path path{R"(/Users/danielhuynh)"};
@@ -58,19 +98,7 @@ void checkDir(){
 
 }   
 
-void printFolder(const string dir_path){
-    const fs::path path{dir_path};
-    //ignore trash
-    if(path.filename() == ".Trash") return;
-    
-    cout <<"\n< -- Interating Through Folder: " << dir_path << " -- >\n\n";
-  
-    fs::directory_entry Directory{path};
-    for(auto const & dir_entry : fs::directory_iterator{Directory}){
-        cout << dir_entry.path() << endl;
-       
-    }
-}
+
 
 int main()
 {
